a104a4d3e85152f43bdf3374256d28a907d4f7ad
LAPTOP-SoonSem <qkeh4161@naver.com>
2018-03-23 18:31:48 +0900

    Add Particle

--- Assets/Scripts/Objects/ShotLine.cs
+++ Assets/Scripts/Objects/ShotLine.cs
@@ -71,7 +71,7 @@ public class ShotLine : MonoBehaviour
 	// 자동 파괴
 	private IEnumerator TimeDestroy()
 	{
-		yield return new WaitForSeconds(2f);
+		yield return new WaitForSeconds(1.5f);
 
 		Destroy(gameObject);
 	}
