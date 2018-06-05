26e79f51c0de7561940a62e207793497f3f6f126
LAPTOP-SoonSem <qkeh4161@naver.com>
2018-06-05 11:01:52 +0900

    Add slide way fade effect

--- Assets/Scripts/UI/CoverSlider.cs
+++ Assets/Scripts/UI/CoverSlider.cs
@@ -7,9 +7,13 @@ namespace UI
 	public class CoverSlider : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 	{
 		// 인스펙터 노출 변수
+		// 일반
+		[SerializeField]
+		private Text[] 			slideWayText;			// 방향 텍스트
+			
 		// 수치
-		public int 				disValue = 3000;		// 거리에 따른 페이드 비율
-		public float 			slideDisPer = 0.5f;		// 슬라이드 거리 비율
+		public  int 			disValue = 3000;		// 거리에 따른 페이드 비율
+		public  float 			slideDisPer = 0.5f;		// 슬라이드 거리 비율
 		
 		// 인스펙터 비노출 변수
 		// 일반
@@ -31,9 +35,9 @@ namespace UI
 			thisImg  	= GetComponent<Image>();
 //			originPos 	= thisRect.position;
 			startPos 	= thisRect.position;
-			imgColor    = thisImg.color;
+			imgColor    = Color.white;
 			midPos 		= new Vector2(Screen.width / 2f, Screen.height / 2f);
-			originAlpha = imgColor.a;
+			originAlpha = 0;
 			slideDis 	= (int)(midPos.x * slideDisPer);
 			
 			Debug.Log(slideDis);
@@ -50,32 +54,37 @@ namespace UI
 		public void OnDrag(PointerEventData eventData)
 		{
 			// 알파 조절
-			imgColor.a = 1 - (Vector2.Distance(eventData.position, startPos) / disValue) - originAlpha / 2;
-			thisImg.color = imgColor;
+			imgColor.a = (Vector2.Distance(eventData.position, startPos) - slideDis) / disValue;
 			
 //			// 위치 조절
 //			thisRect.position -= (Vector3)(previousPos - eventData.position);
 //			previousPos = eventData.position;
 			
+			Debug.Log(imgColor.a);
+			
 			// 방향 측정
 			// 왼쪽
 			if (eventData.position.x < startPos.x - slideDis)
 			{
+				slideWayText[0].color = imgColor;
 				Debug.Log("left");
 			}
 			// 오른쪽
 			if (eventData.position.x > startPos.x + slideDis)
 			{
+				slideWayText[1].color = imgColor;
 				Debug.Log("right");	
 			}
 			// 위쪽
 			if (eventData.position.y > startPos.y + slideDis)
 			{	
+				slideWayText[2].color = imgColor;
 				Debug.Log("up");
 			}
 			// 아래쪽
 			if (eventData.position.y < startPos.y - slideDis)
 			{	
+				slideWayText[3].color = imgColor;
 				Debug.Log("down");
 			}
 		}
@@ -83,8 +92,12 @@ namespace UI
 		// 드래그 종료
 		public void OnEndDrag(PointerEventData eventData)
 		{
-			StartCoroutine(UIEffecter.instance.FadeAlpha(thisImg, originAlpha, 0.1f, false, false));
-//			StartCoroutine(UIEffecter.instance.FadePosition(thisRect, originPos, 0.1f, false, false));
+			for (int i = 0; i < 4; i++)
+			{
+				StartCoroutine(UIEffecter.instance.FadeAlpha(slideWayText[i], originAlpha, 0.1f, false, false));	
+			}
+			
+			//StartCoroutine(UIEffecter.instance.FadePosition(thisRect, originPos, 0.1f, false, false));
 		}
 	}
 }
