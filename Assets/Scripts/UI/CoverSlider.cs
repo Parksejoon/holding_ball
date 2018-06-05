e18f7042e077015fe3dbbc9e0c9b036d110d67e0
LAPTOP-SoonSem <qkeh4161@naver.com>
2018-06-05 19:25:14 +0900

    Add slide each way position fade effect

--- Assets/Scripts/UI/CoverSlider.cs
+++ Assets/Scripts/UI/CoverSlider.cs
@@ -9,7 +9,7 @@ namespace UI
 		// 인스펙터 노출 변수
 		// 일반
 		[SerializeField]
-		private Text[] 			slideWayText;			// 방향 텍스트
+		private Image[] 		slideWayImg;			// 방향별 이미지
 			
 		// 수치
 		public  int 			disValue = 3000;		// 거리에 따른 페이드 비율
@@ -18,86 +18,115 @@ namespace UI
 		// 인스펙터 비노출 변수
 		// 일반
 		private RectTransform 	thisRect;				// 이 이미지의 rect transform
-		private Image 			thisImg;				// 이 이미지
-//		private Vector2 		originPos;				// 최초 위치
-		private Vector2 		startPos;				// 시작 위치
-//		private Vector2 		previousPos;			// 이전 위치
-		private Vector2 		midPos;					// 중앙 위치
 		private Color 			imgColor;				// 이미지 색
-		private float 			originAlpha;			// 최초 알파
-		private int 			slideDis;				// 슬라이드 거리
+		private RectTransform[]	slideWayRect;			// 방향별 rect transform
+		private Vector2[] 		slideWayOriginPos;		// 방향별 원래 위치
+		private bool[] 			isSliding;				// 슬라이드중인지				
 		
+		// 수치
+		private int 			slideMovePer = 1;		// 슬라이드시 움직이는 비율
+		private Vector2 		startPos;				// 시작 위치
+		private Vector2 		midPos;					// 중앙 위치
+		private int 		 	slideDis;				// 슬라이드 거리
+		private float 			originAlpha;			// 최초 알파
 
 		// 초기화
 		private void Awake()
 		{
 			thisRect 	= GetComponent<RectTransform>();
-			thisImg  	= GetComponent<Image>();
-//			originPos 	= thisRect.position;
-			startPos 	= thisRect.position;
 			imgColor    = Color.white;
-			midPos 		= new Vector2(Screen.width / 2f, Screen.height / 2f);
-			originAlpha = 0;
-			slideDis 	= (int)(midPos.x * slideDisPer);
+			isSliding 	= new bool[4] { true, true, true, true };
 			
-			Debug.Log(slideDis);
+			// 배열 변수 초기화
+			slideWayRect = new RectTransform[4];
+			for (int i = 0; i < 4; i++)
+			{
+				slideWayRect[i] = slideWayImg[i].gameObject.GetComponent<RectTransform>();
+			}
+			
+			slideWayOriginPos = new Vector2[4];
+			for (int i = 0; i < 4; i++)
+			{
+				slideWayOriginPos[i] = slideWayRect[i].position;
+			}
+			
+			startPos 	= thisRect.position;
+			midPos 		= new Vector2(Screen.width / 2f, Screen.height / 2f);
+			slideDis 	= (int)(midPos.x * slideDisPer);
+			originAlpha = 0;
 		}
 
 		// 드래그 시작
 		public void OnBeginDrag(PointerEventData eventData)
 		{
-			startPos 	= eventData.position;
-//			previousPos = startPos;
+			startPos = eventData.position;
 		}
 		
 		// 드래그 중
 		public void OnDrag(PointerEventData eventData)
 		{
 			// 알파 조절
-			imgColor.a = (Vector2.Distance(eventData.position, startPos) - slideDis) / disValue;
+			float pointerDis = Vector2.Distance(eventData.position, startPos);
 			
-//			// 위치 조절
-//			thisRect.position -= (Vector3)(previousPos - eventData.position);
-//			previousPos = eventData.position;
+			imgColor.a = (pointerDis - slideDis) / disValue;
 			
 			Debug.Log(imgColor.a);
 			
 			// 방향 측정
 			// 왼쪽
-			if (eventData.position.x < startPos.x - slideDis)
+			if (eventData.position.x < startPos.x - slideDis && isSliding[0])
 			{
-				slideWayText[0].color = imgColor;
-				Debug.Log("left");
+				slideWayImg[0].color = imgColor;
+				slideWayRect[0].position = Vector2.Lerp(slideWayOriginPos[0], midPos, Mathf.Max(0, imgColor.a / slideMovePer));
+
+				isSliding[1] = isSliding[2] = isSliding[3] = false;			
 			}
 			// 오른쪽
-			if (eventData.position.x > startPos.x + slideDis)
+			if (eventData.position.x > startPos.x + slideDis && isSliding[1])
 			{
-				slideWayText[1].color = imgColor;
-				Debug.Log("right");	
+				slideWayImg[1].color = imgColor;
+				slideWayRect[1].position = Vector2.Lerp(slideWayOriginPos[1], midPos, Mathf.Max(0, imgColor.a / slideMovePer));
+				
+				isSliding[0] = isSliding[2] = isSliding[3] = false;
 			}
 			// 위쪽
-			if (eventData.position.y > startPos.y + slideDis)
+			if (eventData.position.y > startPos.y + slideDis && isSliding[2])
 			{	
-				slideWayText[2].color = imgColor;
-				Debug.Log("up");
+				slideWayImg[2].color = imgColor;
+				slideWayRect[2].position = Vector2.Lerp(slideWayOriginPos[2], midPos, Mathf.Max(0, imgColor.a / slideMovePer));
+				
+				isSliding[0] = isSliding[1] = isSliding[3] = false;
 			}
 			// 아래쪽
-			if (eventData.position.y < startPos.y - slideDis)
+			if (eventData.position.y < startPos.y - slideDis && isSliding[3])
 			{	
-				slideWayText[3].color = imgColor;
-				Debug.Log("down");
+				slideWayImg[3].color = imgColor;
+				slideWayRect[3].position = Vector2.Lerp(slideWayOriginPos[3], midPos, Mathf.Max(0, imgColor.a / slideMovePer));
+				
+				isSliding[0] = isSliding[1] = isSliding[2] = false;
 			}
 		}
 
 		// 드래그 종료
 		public void OnEndDrag(PointerEventData eventData)
 		{
+			// 알파 원래대로
 			for (int i = 0; i < 4; i++)
 			{
-				StartCoroutine(UIEffecter.instance.FadeAlpha(slideWayText[i], originAlpha, 0.1f, false, false));	
+				StartCoroutine(UIEffecter.instance.FadeAlpha(slideWayImg[i], originAlpha, 0.1f, false, false));	
 			}
-			
-			//StartCoroutine(UIEffecter.instance.FadePosition(thisRect, originPos, 0.1f, false, false));
+
+			// 위치 원래대로
+			for (int i = 0; i < 4; i++)
+			{
+				StartCoroutine(UIEffecter.instance.FadePosition(slideWayRect[i], slideWayOriginPos[i], 0.1f, false, false));
+			}
+
+			// 플래그 삭제
+			for (int i = 0; i < 4; i++)
+			{
+				isSliding[i] = true;
+			}
 		}
 	}
 }
