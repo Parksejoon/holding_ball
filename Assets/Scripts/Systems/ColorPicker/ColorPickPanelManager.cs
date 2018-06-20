72ed470b6ef0ef069b3c41403f1c4566c556526b
LAPTOP-SoonSem <qkeh4161@naver.com>
2018-06-20 13:35:36 +0900

    Fix color picker slide bug

--- Assets/Scripts/Systems/ColorPicker/ColorPickPanelManager.cs
+++ Assets/Scripts/Systems/ColorPicker/ColorPickPanelManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ColorPickPanelManager : MonoBehaviour
 {
@@ -10,9 +11,12 @@ public class ColorPickPanelManager : MonoBehaviour
 	[SerializeField]
 	private CoverSlider pickCoverSlider;        // 컬러피커 커버 슬라이더
 	[SerializeField]
-	private GameObject	colorPicker;			// 컬러 피커
+	private GameObject	colorPicker;            // 컬러 피커
 
 	// 인스펙터 비노출 변수
+	// 일반
+	private Image		pickCoverSliderImg;		// 컬러피커 커버 슬라이더 이미지
+
 	// 수치
 	private bool		isColorPicking;         // 컬러 피커가 열려있는 상태인가
 	private Vector2		colorPickerOriginPos;   // 컬러 피커 원래 위치
@@ -21,7 +25,9 @@ public class ColorPickPanelManager : MonoBehaviour
 	// 초기화
 	private void Awake()
 	{
-		colorPickerOriginPos = colorPicker.transform.position;
+		pickCoverSliderImg		= pickCoverSlider.GetComponent<Image>();
+
+		colorPickerOriginPos	= colorPicker.transform.position;
 	}
 
 	// 시작 초기화
@@ -40,15 +46,17 @@ public class ColorPickPanelManager : MonoBehaviour
 	// 컬러 피커 열기 루틴
 	private IEnumerator OnOffColorPickerRoutine()
 	{
-		if (isColorPicking)
+		if (!isColorPicking)
 		{
-			// 패널 중앙으로
+			// 패널 중앙으로 & 슬라이더 온
 			UIEffecter.instance.FadeEffect(colorPicker, UIManager.instance.midPos, 0.2f, UIEffecter.FadeFlag.POSITION);
+			pickCoverSliderImg.raycastTarget = true;
 		}
 		else
 		{
-			// 패널 사이드로
+			// 패널 사이드로 & 슬라이더 오프
 			UIEffecter.instance.FadeEffect(colorPicker, colorPickerOriginPos, 0.2f, UIEffecter.FadeFlag.POSITION);
+			pickCoverSliderImg.raycastTarget = false;
 		}
 
 		isColorPicking = !isColorPicking;
