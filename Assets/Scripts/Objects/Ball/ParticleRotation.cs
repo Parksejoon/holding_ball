4ca88f9bce63bfa929f217e3d2b23e0558c9a7db
LAPTOP-SoonSem <qkeh4161@naver.com>
2018-06-23 18:32:39 +0900

    Add particle shower

---
+++ Assets/Scripts/Objects/Ball/ParticleRotation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class ParticleRotation : MonoBehaviour
+{
+	// 인스펙터 노출 변수
+	// 수치
+	[SerializeField]
+	private float speed;            // 회전 속도
+
+
+	// 프레임
+	private void FixedUpdate()
+	{
+		transform.Rotate(Vector3.forward * speed);
+	}
+}
