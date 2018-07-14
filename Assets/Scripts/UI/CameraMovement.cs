b1edcaa8bd9fdd432c15d126716654fdd1303481
LAPTOP-SoonSem <qkeh4161@naver.com>
2018-07-14 20:17:33 +0900

    Change CameraMovement effect script

--- Assets/Scripts/UI/CameraMovement.cs
+++ Assets/Scripts/UI/CameraMovement.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
@@ -14,22 +15,48 @@ public class CameraMovement : MonoBehaviour
 	// 인스펙터 비노출 변수
 	// 수치
 	private float 		moveDis;			// 움직임 정도
-	private Vector2 	newPos;				// 새로운 좌표
-		
-		
-	// 프레임
-	private void FixedUpdate()
-	{
-		newPos = -Camera.main.ScreenToWorldPoint(Input.mousePosition) * 0.1f;
+	private Vector2 	newPos;             // 새로운 좌표
+	
 
-		if (newPos.x < limitPos.x && newPos.x > -limitPos.x)
+	// 프레임
+	private void Update()
+	{
+		if (Input.touchCount > 0)
 		{
-			camera.transform.position = new Vector3(camera.transform.position.x, newPos.y, UIManager.instance.cameraZpos);
+			if (Input.GetTouch(0).phase == TouchPhase.Began)
+			{
+				Debug.Log("asd");
+				StopCoroutine("CameraMove");
+
+				newPos = -Camera.main.ScreenToWorldPoint(Input.mousePosition) * 0.1f;
+
+				if (newPos.x < limitPos.x && newPos.x > -limitPos.x)
+				{
+					//camera.transform.position = new Vector3(camera.transform.position.x, newPos.y, UIManager.instance.cameraZpos);
+					StartCoroutine(CameraMove(new Vector3(camera.transform.position.x, newPos.y, UIManager.instance.cameraZpos)));
+				}
+
+				if (newPos.y < limitPos.y && newPos.y > -limitPos.y)
+				{
+					//camera.transform.position = new Vector3(newPos.x, camera.transform.position.y, UIManager.instance.cameraZpos);	
+					StartCoroutine(CameraMove(new Vector3(newPos.x, camera.transform.position.y, UIManager.instance.cameraZpos)));
+				}
+			}
 		}
-			
-		if (newPos.y < limitPos.y && newPos.y > -limitPos.y)
+	}
+
+	// 카메라 이동 코루틴
+	private IEnumerator CameraMove(Vector3 targetPos)
+	{
+		float moveValue = 0;
+
+		while (moveValue < 1)
 		{
-			camera.transform.position = new Vector3(newPos.x, camera.transform.position.y, UIManager.instance.cameraZpos);	
+			camera.transform.position = Vector3.Lerp(camera.transform.position, targetPos, moveValue);
+
+			moveValue += 0.03f;
+
+			yield return new WaitForSeconds(0.01f);
 		}
 	}
 }
