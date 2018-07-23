b3fa81428d3831e8931a2332d75f534c0e4893fe
LAPTOP-SoonSem <qkeh4161@naver.com>
2018-07-23 09:22:28 +0900

    Add StarScore slide function

---
+++ Assets/Scripts/Systems/Shop/GooglePlayManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class GooglePlayManager : MonoBehaviour
+{
+	// 인스펙터 노출 변수
+	// 일반
+	[SerializeField]
+	private CoverSlider coverSlider;        // 커버 슬라이더
+
+
+	// 시작
+	private void Start()
+	{
+		coverSlider.slideFuncs[0] = GoStarScore;
+	}
+
+	// 별점 메뉴 열기
+	public void GoStarScore()
+	{
+		Debug.Log("ASD");
+
+		// 도전과제 클리어
+		ChallengeManager.instance.ClearChallenge(8);
+	}
+}
