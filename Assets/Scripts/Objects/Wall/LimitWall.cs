4f89c4589d7f9e4d564a38346ba1b329a315ab38
LAPTOP-SoonSem <qkeh4161@naver.com>
2019-04-26 17:12:46 +0900

    Fix limit walls bug

--- Assets/Scripts/Objects/Wall/LimitWall.cs
+++ Assets/Scripts/Objects/Wall/LimitWall.cs
@@ -3,13 +3,13 @@
 public class LimitWall : MonoBehaviour
 {
 	// 충돌
-	private void OnCollisionEnter2D(Collision2D collision)
+	private void OnTriggerEnter2D(Collider2D collision)
 	{
 		// 공은 원상태로 복구
-		if (collision.gameObject.CompareTag("Ball"))
+		if (collision.CompareTag("Ball"))
 		{
 			collision.transform.position = Vector3.zero;
-			collision.rigidbody.velocity = Vector3.zero;
+			collision.GetComponent<Rigidbody2D>().velocity = Vector3.zero;
 		}
 
 		// 홀더와 코인은 파괴처리
